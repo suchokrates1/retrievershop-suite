c54da351b540522abf606e69ed638cdc61ffd086
suchokrates1 <dawsuchodolski@gmail.com>
2025-07-02 17:38:09 +0200

    Add monthly report toggle

--- magazyn/env_info.py
+++ magazyn/env_info.py
@@ -67,4 +67,8 @@ ENV_INFO = {
     "SMTP_PORT": ("Port SMTP", "Port serwera SMTP"),
     "SMTP_USERNAME": ("Użytkownik SMTP", "Login do serwera SMTP"),
     "SMTP_PASSWORD": ("Hasło SMTP", "Hasło do serwera SMTP"),
+    "ENABLE_MONTHLY_REPORTS": (
+        "Raport miesięczny",
+        "Wysyłaj raport miesięczny",
+    ),
 }
