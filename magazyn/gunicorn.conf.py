c44fb4430ed76b6edc4a88f0e892a399d33bd200
suchokrates1 <dawsuchodolski@gmail.com>
2025-12-29 18:32:10 +0100

    Change Gunicorn worker to gevent

--- magazyn/gunicorn.conf.py
+++ magazyn/gunicorn.conf.py
@@ -1,7 +1,7 @@
 # gunicorn.conf.py
 bind = "0.0.0.0:8000"
 workers = 2  # Number of worker processes
-worker_class = 'sync'  # Standard synchronous worker
+worker_class = 'gevent'  # Gevent worker without websocket
 timeout = 120  # Worker timeout in seconds
 keepalive = 5  # Keep-alive connections
 graceful_timeout = 30  # Graceful worker restart timeout
