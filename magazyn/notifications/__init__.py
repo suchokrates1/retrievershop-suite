aea70eb1b0bcdaeee48dfb8a0f421ee3bbb029ba
suchokrates1 <dawsuchodolski@gmail.com>
2026-01-27 14:24:31 +0100

    Fix: przeniesienie send_stock_alert do notifications/alerts.py
    - Utworzono alerts.py z funkcjami send_stock_alert i send_email
    - Usunieto stary notifications.py (zastapiony katalogiem)
    - Dodano fallback w send_messenger przez settings (kompatybilnosc)

--- magazyn/notifications/__init__.py
+++ magazyn/notifications/__init__.py
@@ -4,6 +4,7 @@ Modul powiadomien - centralizuje wysylanie wiadomosci.
 
 from .messenger import send_messenger, MessengerClient
 from .reports import send_report, format_period_report, ReportGenerator
+from .alerts import send_stock_alert, send_email
 
 __all__ = [
     'send_messenger',
@@ -11,4 +12,6 @@ __all__ = [
     'send_report',
     'format_period_report',
     'ReportGenerator',
+    'send_stock_alert',
+    'send_email',
 ]
