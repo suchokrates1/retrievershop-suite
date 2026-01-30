5ef58f2f41f7e0c2fcddcd998aa7058800ef5e51
suchokrates1 <dawsuchodolski@gmail.com>
2026-01-31 00:13:16 +0100

    Dodanie migracji price_reports do entrypoint.sh

--- magazyn/entrypoint.sh
+++ magazyn/entrypoint.sh
@@ -7,5 +7,11 @@ echo "Running database migrations..."
 cd /app
 alembic upgrade head
 
+echo "Running custom migrations..."
+python -c "
+from magazyn.migrations.create_price_reports_tables import run_migration
+run_migration()
+"
+
 echo "Starting Gunicorn..."
 exec gunicorn magazyn.wsgi:app --bind 0.0.0.0:8000 --config magazyn/gunicorn.conf.py
